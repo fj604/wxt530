43cb4ff4ac2c0d0436d908e281cc306ae4a9c29d
fj604 <22716793+fj604@users.noreply.github.com>
2020-03-10 01:11:32 +0000

    foormatting

--- wxt530.py
+++ wxt530.py
@@ -8,10 +8,13 @@ import sys
 import paho.mqtt.client as mqtt
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("-p", "--port", default="/dev/ttyUSB0", help="Serial port")
-argparser.add_argument("-b", "--baud", default=9600, type=int, help="Baud rate")
+argparser.add_argument(
+    "-p", "--port", default="/dev/ttyUSB0", help="Serial port")
+argparser.add_argument("-b", "--baud", default=9600,
+                       type=int, help="Baud rate")
 argparser.add_argument("-j", "--json", action="store_true", help="JSON output")
-argparser.add_argument("-m", "--mqtt", default="localhost", help="MQTT broker hostname")
+argparser.add_argument("-m", "--mqtt", default="localhost",
+                       help="MQTT broker hostname")
 argparser.add_argument("-t", "--topic", type=str, help="MQTT topic")
 
 args = argparser.parse_args()
@@ -25,15 +28,19 @@ header = "$WIXDR"
 
 t = {}
 
-t['A'] = {0: "Wind direction min", 1: "Wind direction average", 2: "Wind direction max"}
+t['A'] = {0: "Wind direction min",
+          1: "Wind direction average", 2: "Wind direction max"}
 t['S'] = {0: "Wind speed min", 1: "Wind speed average", 2: "Wind speed max"}
 t['P'] = {0: "Pressure"}
-t['C'] = {0: "Air temperature", 1: "Internal temperature", 2: "Heating temperature", 3: "Aux. temperature (pt1000)"}
+t['C'] = {0: "Air temperature", 1: "Internal temperature",
+          2: "Heating temperature", 3: "Aux. temperature (pt1000)"}
 t['H'] = {0: "Relative humidity"}
 t['V'] = {0: "Rain accumulation", 1: "Hail accumulation"}
 t['Z'] = {0: "Rain duration", 1: "Hail duration"}
-t['R'] = {0: "Rain current intensity", 1: "Hail current intensity", 2: "Rain peak intensity", 3: "Hail peak intensity"}
-t['U'] = {0: "Supply voltage", 1: "Heating voltage", 2: "3.5 V reference voltage", 3: "Solar radiation", 4: "Ultrasonic level sensor"}
+t['R'] = {0: "Rain current intensity", 1: "Hail current intensity",
+          2: "Rain peak intensity", 3: "Hail peak intensity"}
+t['U'] = {0: "Supply voltage", 1: "Heating voltage", 2: "3.5 V reference voltage",
+          3: "Solar radiation", 4: "Ultrasonic level sensor"}
 t['G'] = {4: "Information field"}
 
 u = {}
@@ -41,7 +48,7 @@ u = {}
 u['C'] = {'C': "Celsius", 'F': "Fahrenheit"}
 u['A'] = {'D': "degrees"}
 u['S'] = {'K': "km/h", 'M': "m/s", 'N': "knots", 'S': "mph"}
-u['P'] = {'B': "bars", 'P': "Pascal", 'H': "hPa", 'I': "inHg", 'M': "mmHg" }
+u['P'] = {'B': "bars", 'P': "Pascal", 'H': "hPa", 'I': "inHg", 'M': "mmHg"}
 u['H'] = {'P': "Percent"}
 u['V'] = {'M': "mm", 'I': "in", 'H': "hits"}
 u['Z'] = {'S': "seconds"}
@@ -80,7 +87,8 @@ with serial.Serial(port, baudrate, timeout=timeout) as file:
                     data = 0
                 units = items[i + 2].upper()
                 t_id = int(items[i + 3])
-                m.append({"measurement": t[t_type][t_id], "data": data, "units": u[t_type][units]})
+                m.append(
+                    {"measurement": t[t_type][t_id], "data": data, "units": u[t_type][units]})
                 if not args.json:
                     print(t[t_type][t_id], data, u[t_type][units])
                 i += 4
@@ -88,6 +96,7 @@ with serial.Serial(port, baudrate, timeout=timeout) as file:
                 print(json.dumps(m))
             if args.topic is not None:
                 for measurement in m:
-                    topic = args.topic + "/" + measurement["measurement"].replace(' ', '_')
+                    topic = args.topic + "/" + \
+                        measurement["measurement"].replace(' ', '_')
                     client.publish(topic, measurement["data"])
             sys.stdout.flush()
